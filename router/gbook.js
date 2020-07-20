043f01e7e93142c64947f9214cd6b9f76e822433
ilkwonKIM <ilkim226@gmail.com>
2020-07-20 16:34:28 +0900

    pager

--- router/gbook.js
+++ router/gbook.js
@@ -1,24 +1,40 @@
 const express = require('express');
 const router = express.Router();
-const { pool, mysqlErr } = require('../modules/mysql-conn');
-const moment = require('moment')
+const { pool, mysqlErr } = require('../modules/mysql.conn');
+const moment = require('moment');
+const pagerInit = require('../modules/pager.conn');
 
-router.get('/',async (req, res, next) => {
-	let connect, sql, sqlVal, result,lists;
-	try{
-		sql = 'SELECT * FROM gbook ORDER BY id DESC'
-		connect = await pool.getConnection()
+// 127.0.0.1:3000/gbook
+// 127.0.0.1:3000/gbook?cnt=10
+// 127.0.0.1:3000/gbook/list
+// 127.0.0.1:3000/gbook/list?cnt=10
+// 127.0.0.1:3000/gbook/list/3
+// 127.0.0.1:3000/gbook/list/3?cnt=10
+// console.log(page, cnt, stRec);
+router.get(['/', '/list', '/list/:page'], async (req, res, next) => {
+	let connect, sql, sqlVal, result, pager = {}; 
+	try {
+		connect = await pool.getConnection();
+		sql = 'SELECT COUNT(id) FROM gbook';
 		result = await connect.execute(sql);
-		lists = result [0];
+		/******* pager.start ******/
+		pager.path = '/gbook/list'
+		pager.page = Number(req.params.page || 1); 
+		pager.cnt = Number(req.query.cnt || 5);
+		pager.grp = Number(req.query.grp || 3);
+		pager.totalRec = result[0][0]['COUNT(id)'];
+		pager = pagerInit(pager);
+		sql = 'SELECT * FROM gbook ORDER BY id DESC LIMIT ?, ?';
+		sqlVal = [pager.stRec, pager.cnt];
+		result = await connect.execute(sql, sqlVal);
 		connect.release();
 		for(let v of result[0]) v.createdAt = moment(v.createdAt).format('YYYY-MM-DD hh:mm:ss');
-		const pug = { css: 'gbook', js: 'gbook',lists: result[0] };
+		const pug = { css: 'gbook', js: 'gbook', lists: result[0], pager };
 		res.render('gbook/gbook.pug', pug);
 	}
 	catch(e) {
-		next(e);
+		next(mysqlErr(e));
 	}
-	
 });
 
 router.post('/save', async (req, res, next) => {
@@ -29,11 +45,9 @@ router.post('/save', async (req, res, next) => {
 		sqlVal = [writer, comment];
 		connect = await pool.getConnection();
 		result = await connect.execute(sql, sqlVal);
-		connect.release();
 		res.redirect('/gbook');
 	}
 	catch(e) {
-		connect.release();
 		next(mysqlErr(e));
 	}
 });
