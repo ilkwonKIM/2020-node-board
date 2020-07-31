72a73bafa306a79d2669b752eb74c449c8ad5094
ilkwonKIM <ilkim226@gmail.com>
2020-07-31 14:51:39 +0900

    gallery-il & gridstack

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,17 +1,20 @@
-var grid = GridStack.init();
+var grid = GridStack.init({
+	animate: true,
+});
 
 
-function onGallerySubmit (f) {
-	var title = f.title.value.trim() 
-	var writer = f.writer.value.trim() 
+
+function onGallerySubmit(f) {
+	var title = f.title.value.trim();
+	var writer = f.writer.value.trim();
 	if(title == '') {
 		alert("제목을 작성해 주세요.");
-		f.title.focus()
+		f.title.focus();
 		return false;
 	}
 	if(writer == '') {
-		alert("작성자를 작성해 주세요.");
-		f.writer.focus()
+		alert("작성자를 기재해 주세요.");
+		f.writer.focus();
 		return false;
 	}
 	return true;
