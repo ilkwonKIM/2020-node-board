dd77ff7015d93dd72c72935e411d6f1e19e4fa43
ilkwonKIM <ilkim226@gmail.com>
2020-08-06 13:20:47 +0900

    404Error

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+	if(error.code !=404) console.log(error);
 	console.log(error);
 	const code = error.code || 500;
 	const msg = error.msg || '서버 내부 오류 입니다. 관리자에게 문의하세요.';
